42ac2ad27519ce7fc891562d8fb7b8a8a2cabc2b
Chris Reickenbacker <chris@serverless-solutions.com>
2023-01-24 03:14:09 -0600

    initial commit

---
+++ hellocontainers/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using hellocontainers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hellocontainers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<HelloResponse> GetResult(string? echo)
+        {
+            return new HelloResponse(Message, YouSaid: echo ?? "Nothing");
+        }
+
+        [HttpPost]
+        public ActionResult<HelloResponse> PostResult([FromBody]HelloRequest request)
+        {
+            return new HelloResponse(Message, YouSaid: request.Echo ?? "Nothing");
+        }
+
+        private string Message => Environment.GetEnvironmentVariable("HELLOCONTAINERS_MESSAGE") ?? "No Message Set!";
+    }
+}
